03f54320d9962a85ce4f71080337f42d481a3244
BouChrahns99 <bouchrahannnasi5@gmail.com>
2023-10-10 04:49:52 -0700

    Input/Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
-"""
-This program define a Student in a class
-"""
+""" defines a Student class """
 
 
-class Student():
-    """
-    Class of a student
-    """
-
+class Student:
+    """ Student class """
     def __init__(self, first_name, last_name, age):
-        """
-        Constructor of a student
-        Args:
-          - first_name: str
-          - last_name: str
-          - age: int
-        """
+        """ instance initializer """
+        if type(first_name) is not str:
+            raise TypeError("first_name must be a string")
+        if len(first_name) > 10:
+            raise ValueError
         self.first_name = first_name
+        if type(last_name) is not str:
+            raise TypeError("last_name must be a string")
+        if len(last_name) > 10:
+            raise ValueError
         self.last_name = last_name
+        if age < 0:
+            raise ValueError("age cannot be less than 0")
         self.age = age
 
-    def to_json(self, attrs=None):
-        """
-        Return the dict representation of the instance
-        Args:
-          - attrs: list (None default)
-        """
-
-        result = {}
-
-        if attrs is None:
-            return (self.__dict__)
-
-        for attr in attrs:
-            value = self.__dict__.get(attr)
-            if value is not None:
-                result[attr] = value
-
-        return (result)
+    def to_json(self, attr=None):
+        """ retrives a dictionary representation of Student object """
+        if type(attr) is list:
+            for item in attr:
+                if type(item) is not str:
+                    break
+            dict_attr = {}
+            for key, value in self.__dict__.items():
+                if key in attr:
+                    dict_attr[key] = value
+            return dict_attr
+        return self.__dict__
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student class """
